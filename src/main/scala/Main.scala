445ed9897942ca6ae6a1b100b4fc87bafd6a9215
Wassim Labdi <labdiwassim18@gmail.com>
2023-03-12 12:57:59 +0100

    Test de src

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,267 +1,112 @@
 
-import Common.IO
-import Common.Tools._
-import breeze.linalg.{DenseMatrix, DenseVector}
-import breeze.stats.distributions.Gamma
+import breeze.linalg.DenseVector
+import org.apache.log4j.PropertyConfigurator
+import org.apache.spark.SparkContext
+import org.apache.spark.api.java.JavaRDD.fromRDD
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import utils.{DataGenerator, NamedVector}
 
+import java.security.Permission
+import java.util.Random
+
+
+
+
+
+//import org.altic.spark.clustering.utils.{NamedVector, DataGenerator}
+
+//object RunBiTM extends App {
 object Main {
 
-  implicit val ss: SparkSession = SparkSession
-    .builder()
-    .master("local[*]")
-    .appName("AnalysePlan")
-    .config("spark.executor.cores", 2)
-    //.config("spark.executor.memory", "30G")
-    .config("spark.executor.heartbeatInterval", "20s")
-    .config("spark.driver.memory", "10G")
-    .getOrCreate()
-
-  ss.sparkContext.setLogLevel("WARN")
-  ss.sparkContext.setCheckpointDir("checkpointDir")
-
+  val conf = new SparkConf().setAppName("Mon application Spark").setMaster("local[*]")
+  val sc = new SparkContext(conf)
   def main(args: Array[String]) {
-
-    val shape = 1E1
-    val scale = 2E1
-
-    val alphaPrior = Some(Gamma(shape = shape, scale = scale)) // lignes
-    val betaPrior = Some(Gamma(shape = shape, scale = scale)) // clusters redondants
-
-    val trueRowPartitionSize = List(20,30,40,30,20)
-    val trueColPartitionSize = List(40,20,30,20,30)
-    val trueBlockPartition = getBlockPartition(getPartitionFromSize(trueRowPartitionSize),
-      getPartitionFromSize(trueColPartitionSize))
-
-    val dataMatrix = Common.IO.readDenseMatrixDvDouble("src/main/scala/dataset.csv")
-    val dataList = matrixToDataByCol(dataMatrix)
-
-    val nIter = 5
-    val verbose = false
-    val nLaunches = 1
+    println("Hey");
 
 
-    println("Benchmark begins. It is composed of "+ nLaunches.toString + " launches, each launch runs every methods once.")
+    val nbRowSOM = 10
+    val nbColSOM = 10
+    val nbIter = 10
+    val dataNbObs = 4
+    val dataNbVars = 2
 
-    (0 until nLaunches).foreach(iter => {
 
-      println("Launch number " + iter)
+    class AffectedVector(var rowNeuronId: Int, elements: Array[Double]) extends DenseVector(elements) {
+      def indices = elements.indices
+    }
 
-      /* var t0 = System.nanoTime()
+    def parseData(line: String): AffectedVector = {
+      // TODO : Remplacer 0 par rand
+      new AffectedVector(1, line.split(' ').map(_.toDouble))
+    }
 
 
 
-     //////////////////////////////////// BGMM_1
-
-     val ((ariBGMM1, riBGMM1, nmiBGMM1, nClusterBGMM1), runtimeBGMM1) = {
-
-       var t0 = System.nanoTime()
-
-       val bestBGMMRow = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(5),
-         rangeCol = List(1),
-         verbose = verbose,
-         nConcurrentEachTest = 1,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMRowPartition = bestBGMMRow("RowPartition").asInstanceOf[List[Int]]
-
-       val bestBGMMCol = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(1),
-         rangeCol = List(5),
-         verbose = verbose,
-         nConcurrentEachTest = 1,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMColPartition = bestBGMMCol("ColPartition").asInstanceOf[List[Int]]
-
-       val t1 = printTime(t0, "BGMM 1")
-
-       val blockPartition = getBlockPartition(bestBGMMRowPartition, bestBGMMColPartition)
-       (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-     }
-
-     //////////////////////////////////// BGMM_14
-
-     val ((ariBGMM14, riBGMM14, nmiBGMM14, nClusterBGMM14), runtimeBGMM14) = {
-
-       var t0 = System.nanoTime()
-
-       val bestBGMMRow = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(5),
-         rangeCol = List(1),
-         verbose = verbose,
-         nConcurrentEachTest = 4,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMRowPartition = bestBGMMRow("RowPartition").asInstanceOf[List[Int]]
-
-       val bestBGMMCol = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(1),
-         rangeCol = List(5),
-         verbose = verbose,
-         nConcurrentEachTest = 4,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMColPartition = bestBGMMCol("ColPartition").asInstanceOf[List[Int]]
-
-       val t1 = printTime(t0, "BGMM 14")
-
-       val blockPartition = getBlockPartition(bestBGMMRowPartition, bestBGMMColPartition)
-       (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-     }
-
-     //////////////////////////////////// BGMM_MS
-
-     val ((ariBGMM_MS, riBGMM_MS, nmiBGMM_MS, nClusterBGMM_MS), runtimeBGMM_MS) = {
-
-       var t0 = System.nanoTime()
-
-       val bestBGMMRow = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(4, 5, 6),
-         rangeCol = List(1),
-         verbose = verbose,
-         nConcurrentEachTest = 1,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMRowPartition = bestBGMMRow("RowPartition").asInstanceOf[List[Int]]
-
-       val bestBGMMCol = FunLBM.ModelSelection.gridSearch(
-         dataMatrix,
-         rangeRow = List(1),
-         rangeCol = List(4, 5, 6),
-         verbose = verbose,
-         nConcurrentEachTest = 1,
-         nTryMaxPerConcurrent = 1)
-       val bestBGMMColPartition = bestBGMMCol("ColPartition").asInstanceOf[List[Int]]
-
-       val t1 = printTime(t0, "BGMM MS")
-
-       val blockPartition = getBlockPartition(bestBGMMRowPartition, bestBGMMColPartition)
-       (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-     }
-     */
-      //////////////////////////////////// LBM1
-
-      val ((ariLBM1, riLBM1, nmiLBM1, nClusterLBM1), runtimeLBM1) = {
-        var t0 = System.nanoTime()
-        val bestLBM = FunLBM.ModelSelection.gridSearch(
-          dataMatrix,
-          rangeRow = List(5),
-          rangeCol = List(5),
-          verbose = verbose,
-          nConcurrentEachTest = 1,
-          nTryMaxPerConcurrent = 5)
-        val (rowMembershipLBM, colMembershipLBM) = (
-          bestLBM("RowPartition").asInstanceOf[List[Int]],
-          bestLBM("ColPartition").asInstanceOf[List[Int]])
-        val t1 = printTime(t0, "LBM 1")
-
-        val blockPartition = getBlockPartition(rowMembershipLBM, colMembershipLBM)
-        (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-      }
-
-      //////////////////////////////////// LBM49
-
-      val ((ariLBM49, riLBM49, nmiLBM49, nClusterLBM49), runtimeLBM49) = {
-        var t0 = System.nanoTime()
-        val bestLBM = FunLBM.ModelSelection.gridSearch(
-          dataMatrix,
-          rangeRow = List(5),
-          rangeCol = List(5),
-          verbose = verbose,
-          nConcurrentEachTest = 10,
-          nTryMaxPerConcurrent = 5)
-        val (rowMembershipLBM, colMembershipLBM) = (
-          bestLBM("RowPartition").asInstanceOf[List[Int]],
-          bestLBM("ColPartition").asInstanceOf[List[Int]])
-        val t1 = printTime(t0, "LBM 49")
-
-        val blockPartition = getBlockPartition(rowMembershipLBM, colMembershipLBM)
-        (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-      }
-
-      //////////////////////////////////// LBM_MS
-
-      val ((ariLBM_MS, riLBM_MS, nmiLBM_MS, nClusterLBM_MS), runtimeLBM_MS) = {
-        var t0 = System.nanoTime()
-        val bestLBM = FunLBM.ModelSelection.gridSearch(
-          dataMatrix,
-          rangeRow = List(4, 5, 6),
-          rangeCol = List(4, 5, 6),
-          verbose = verbose,
-          nConcurrentEachTest = 1,
-          nTryMaxPerConcurrent = 5)
-        val (rowMembershipLBM, colMembershipLBM) = (
-          bestLBM("RowPartition").asInstanceOf[List[Int]],
-          bestLBM("ColPartition").asInstanceOf[List[Int]])
-        val t1 = printTime(t0, "LBM MS")
-
-        val blockPartition = getBlockPartition(rowMembershipLBM, colMembershipLBM)
-        (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-      }
-
-      ////////////////////////////////// BDPMM
-      /*
-
-          val ((ariBDPMM, riBDPMM, nmiBDPMM, nClusterBDPMM), runtimeBDPMM) = {
-            var t0 = System.nanoTime()
-
-            val (rowMembershipsBDPMM, _, _) = new FunNPLBM.CollapsedGibbsSampler(
-              dataList,
-              alphaPrior = alphaPrior,
-              betaPrior = betaPrior,
-              initByUserColPartition = Some(dataList.indices.toList)).runWithFixedPartitions(
-              nIter,
-              verbose = verbose, updateCol = false, updateRow = true)
-
-            val (_, colMembershipsBDPMM, _) = new FunNPLBM.CollapsedGibbsSampler(
-              dataList,
-              alphaPrior = alphaPrior,
-              betaPrior = betaPrior,
-              initByUserRowPartition = Some(dataList.head.indices.toList)
-            ).runWithFixedPartitions(
-              nIter,
-              verbose = verbose, updateCol = true, updateRow = false)
-
-            val t1 = printTime(t0, "BDPMM")
-            val blockPartition = getBlockPartition(rowMembershipsBDPMM.last, colMembershipsBDPMM.last)
-            (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-          }
-          */
-      //////////////////////////////////// NPLBM
-
-      val ((ariNPLBM, riNPLBM, nmiNPLBM, nClusterNPLBM), runtimeNPLBM) = {
-        var t0 = System.nanoTime()
-        val (rowMembershipNPLBM, colMembershipNPLBM, _) = new FunNPLBM.CollapsedGibbsSampler(dataList,
-          alphaPrior = alphaPrior,
-          betaPrior = betaPrior).run(nIter, verbose = verbose)
-        val t1 = printTime(t0, "NPLBM")
-        val blockPartition = getBlockPartition(rowMembershipNPLBM.last, colMembershipNPLBM.last)
-        (getScores(blockPartition, trueBlockPartition), (t1 - t0)/1e9D)
-      }
-
-      val ARIs = Array(shape, scale, ariLBM1, ariLBM49, ariLBM_MS, ariNPLBM)
-      val RIs = Array(shape, scale, riLBM1, riLBM49, riLBM_MS, riNPLBM)
-      val NMIs = Array(shape, scale,  nmiLBM1, nmiLBM49, nmiLBM_MS, nmiNPLBM)
-      val nClusters = Array(shape, scale, nClusterLBM1, nClusterLBM49, nClusterLBM_MS, nClusterNPLBM)
-      val runtimes = Array(shape, scale, runtimeLBM1, runtimeLBM49, runtimeLBM_MS, runtimeNPLBM)
 
 
-      val ARIMat = DenseMatrix(ARIs.map(_.toString)).reshape(1, ARIs.length)
-      val RIMat  = DenseMatrix( RIs.map(_.toString)).reshape(1, ARIs.length)
-      val NMIMat = DenseMatrix(NMIs.map(_.toString)).reshape(1, ARIs.length)
-      val nClusterMat = DenseMatrix(nClusters.map(_.toString)).reshape(1, ARIs.length)
-      val runtimesMat = DenseMatrix(runtimes.map(_.toString)).reshape(1, ARIs.length)
+    // Read data from file
+    //val lines = sc.textFile("/home/tug/ScalaProjects/spark/kmeans_data.txt")
+    //val datas = lines.map(parseData _).cache()
 
-      val append = true
+    //val datas = sc.parallelize(Array.fill(1)(parseData("1")), 1)
 
-      IO.writeMatrixStringToCsv("src/main/scala/ARIs.csv", ARIMat, append=append)
-      IO.writeMatrixStringToCsv("src/main/scala/RIs.csv" , RIMat , append=append)
-      IO.writeMatrixStringToCsv("src/main/scala/NMIs.csv", NMIMat, append=append)
-      IO.writeMatrixStringToCsv("src/main/scala/nClusters.csv", nClusterMat, append=append)
 
+   // val arrDatas = DataGenerator.gen2ClsNDims(dataNbObs, dataNbVars).getNamedVector
+    //val arrDatas = Array(Vector(Array(1.0, 1.1)), Vector(Array(2.0, 2.1)))
+    //val arrDatas = Array.tabulate(dataNbObs)(i => new Vector(Array.tabulate(dataNbVars)(j => i*20+j+10)))
+    //val arrDatas = Array.tabulate(dataNbObs)(i => new AffectedVector(1, Array.tabulate(dataNbVars)(j => 1)))
+    //val datas = sc.parallelize(arrDatas, 6)
+
+
+    val myVector = Vectors.zeros(10000)
+
+
+   //val datas :RDD[NamedVector] = sc.emptyRDD
+
+    /*val datas = sc.
+      textFile("C:\\Users\\33658\\IdeaProjects\\NPLBM\\src\\main\\waveform-5000_csv.csv").map
+    (line => line.split(",").map(_.toDouble))*/
+
+    val datas = sc.textFile("C:\\Users\\33658\\IdeaProjects\\NPLBM\\src\\main\\waveform-5000_csv.csv")
+      .mapPartitionsWithIndex((index, iterator) => if (index == 0) iterator.drop(1) else iterator)
+      .map(line => line.split(",").map(_.toDouble).dropRight(1))
+
+    val premiereLigne = datas.first()
+
+    // Afficher la première valeur de la première ligne
+    datas.collect().foreach(arr => {
+      arr.foreach(element => {
+        println(element)
+      })
     })
+    /*ON DEVRA PEUT ETRE AUSSI ENLEVER LA COLONNE CLASS ( COMME LE PROF AVAIT DIT )*/
+
+
+    /*PEUT ETRE ON POURRA ABANDONNER LE NAMEDVECTOR ?*/
+          //val datas = sc.textFile("/home/tug/pyImg/randImg.4colors.data").map(line => new Vector(line.split("\t").map(_.toDouble)))
+
+
+          // Initialisation du model
+        val model = new BiTM(nbRowSOM, nbColSOM,datas)
+         //val model = new Croeuc(nbRowSOM * nbColSOM, datas)
+
+
+          model.training(nbIter)
+
+          val affData = model.affectation(datas)
+
+
+    affData.first().foreach(element => {
+        println(element)
+      })
+
+       sys.exit()
+          // FAUT MODIF LE MAIN
+
+
+
   }
 }
-
